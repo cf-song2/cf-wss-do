2926cc3220fb432ab4690752361d63225eb203d4
cf-song2 <user.email=cfsong296@gmail.com>
2025-03-17 07:25:01 +0900

    modified

--- src/worker.js
+++ src/worker.js
@@ -5,10 +5,10 @@ export default {
     const upgradeHeader = request.headers.get("Upgrade");
 
     if (upgradeHeader && upgradeHeader.toLowerCase() === "websocket") {
+      console.log("[Worker] Forwarding WebSocket request to DO");
+
       const id = env.CHAT_DO.idFromName("proxy-room");
       const stub = env.CHAT_DO.get(id);
-
-      console.log("[Worker] Forwarding WebSocket request to DO");
       return await stub.fetch(request);
     }
 
